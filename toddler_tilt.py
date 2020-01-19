c2cf72f9f4cc88c2ddb2241143918ae4b8ae2a98
Kaushal Parikh <kaushalp88@gmail.com>
2020-01-19 12:26:50 -0500

    Parse possible moves complete

--- toddler_tilt.py
+++ toddler_tilt.py
@@ -4,12 +4,12 @@ def initialize():
         ['B', 'B', '', '', '', '', '', '', '', '', 'B', 'B'],
         ['', '', 'ML-EU-WR', 'UL-CD-NR', 'OA', 'PU', 'AU', 'UR-CU-NL', 'UD-CR-NU', 'SU-SD', '', ''],
         ['', '', 'FL', 'AU', 'BU', '#', 'OA', 'AD', 'KR', 'WD-ER-MU', '', ''],
-        ['', '', 'GU', 'OA', 'UD-CR-NU', 'ND-UU-CL', '#', 'ML-EU-WR', '#', 'GR', '', ''],
+        ['', '', 'GU', 'OA', 'UD-CR-NU', 'ND-UU-CL', '#A', 'ML-EU-WR', '#A', 'GR', '', ''],
         ['', '', 'YL', 'AL', 'UD-CR-NU', 'SU-SD', 'HU-HD-IL-IR', '#', 'UR-CU-NL', 'UR-CU-NL', '', ''],
-        ['', '', 'PL', '#', 'RL', 'BR', 'SL-SR', 'UR-CU-NL', 'DL', 'RR', '', ''],
-        ['', '', '#', 'FD', 'HU-HD-IL-IR', 'OA', 'ND-UU-CL', 'AR', 'BR', 'OA', '', ''],
-        ['', '', 'GU', 'WD-ER-MU', 'TD', 'OA', 'UL-CU-WR', '#', 'MD-ER-WU', '#', '', ''],
-        ['', '', 'SU-SD', 'OA', '#', 'ND-UU-CL', 'YD', 'MD-ER-WU', 'AD', 'ND-UU-CL', '', ''],
+        ['', '', 'PL', '#A', 'RL', 'BR', 'SL-SR', 'UR-CU-NL', 'DL', 'RR', '', ''],
+        ['', '', '#A', 'FD', 'HU-HD-IL-IR', 'OA', 'ND-UU-CL', 'AR', 'BR', 'OA', '', ''],
+        ['', '', 'GU', 'WD-ER-MU', 'TD', 'OA', 'UL-CU-WR', '#A', 'MD-ER-WU', '#A', '', ''],
+        ['', '', 'SU-SD', 'OA', '#A', 'ND-UU-CL', 'YD', 'MD-ER-WU', 'AD', 'ND-UU-CL', '', ''],
         ['B', 'B', '', '', '', '', '', '', '', '', 'B', 'B'],
         ['B', 'B', '', '', '', '', '', '', '', '', 'B', 'B'],
     ]
@@ -22,6 +22,14 @@ def is_possible(direction, letter):
             return (option[0], True)
     return ('', False)
 
+def update_moves(moves, letter, i, direction):
+    move_string = str(i) + '-' + direction
+    if letter in moves.keys():
+        moves[letter].append(move_string)
+    else:
+        moves[letter] = [move_string]
+    return moves
+
 def parse_possible_moves(board):
     moves = {}
 
@@ -31,15 +39,54 @@ def parse_possible_moves(board):
         for j in range(2, len(board) - 2):
             # board[j][i] as j goes up goes down a row
             current = board[j][i]
-            print current
+            print current, j, i
             if board[1][i] == '' and not clue_found and current != '':
                 letter, valid = is_possible('U', current)
                 if valid:
-                    if letter in moves.keys():
+                    moves = update_moves(moves, letter, i, 'U')
+                    '''if letter in moves.keys():
                         moves[letter].append(str(i) + '-U')
                     else:
-                        moves[letter] = [str(i) + '-U']
+                        moves[letter] = [str(i) + '-U']'''
                 clue_found = True
+
+    #Checking all the downs
+    for i in reversed(range(len(board) - 2)):
+        clue_found = False
+        for j in reversed(range(2, len(board) - 2)):
+            current = board[j][i]
+            if board[10][i] == '' and not clue_found and current != '':
+                print current
+                letter, valid = is_possible('D', current)
+                if valid:
+                    moves = update_moves(moves, letter, i, 'D')
+                clue_found = True
+
+    #Checking all the lefts
+    for i in range(2, len(board)):
+        clue_found = False
+        for j in range(2, len(board) - 2):
+            # board[i][j] as j goes up goes down a row
+            current = board[i][j]
+            print current, j, i
+            if board[1][j] == '' and not clue_found and current != '':
+                letter, valid = is_possible('L', current)
+                if valid:
+                    moves = update_moves(moves, letter, i, 'L')
+                clue_found = True
+
+    #Checking all the lefts
+    for i in reversed(range(len(board) - 2)):
+        clue_found = False
+        for j in reversed(range(2, len(board) - 2)):
+            current = board[i][j]
+            if board[10][j] == '' and not clue_found and current != '':
+                print current
+                letter, valid = is_possible('R', current)
+                if valid:
+                    moves = update_moves(moves, letter, i, 'R')
+                clue_found = True
+
     return moves
 
 
